fadac86dbf9e2a32de782a968e1523052ace170a
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-01-01 14:45:53 +0400

    add some comments to help myself better understand the code

--- flashcards-app/src/CardsList.js
+++ flashcards-app/src/CardsList.js
@@ -1,12 +1,18 @@
-const CardsList = ({ cards, handleDelete }) => {
+//this component renders list of cards using map function
+//'cards' array of card objects 
+//'cars' is an element generated as a result of map iteration
+
+const CardsList = ({ cards, handleDelete, showBack }) => {
     return ( 
         <div className="card-list">
             {cards.map((card) => (
-                <div className="cards" key={card.id}>
+                <div className="cards" key={card.id} front={card.front} back={card.back}>
                     <h4>{card.front}</h4>
                     <div className="button-container">
                         <button className="delete-button" onClick={() => handleDelete(card.id)}>delete</button>
                         <button className="edit-button" >update</button>
+                        {/* <button className="show-button" onClick={() => showBack(card.id)}>SHOW</button> */}
+                        
                     </div>
                     
                 </div>
