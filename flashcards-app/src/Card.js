89ad5d4ebff5c5305a7aa77455d1b34c45f98f66
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-01-01 22:14:28 +0400

    create state for card component to flip the card, create function handleFlip

--- flashcards-app/src/Card.js
+++ flashcards-app/src/Card.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
 const Card = ({ id, front, back, handleDelete }) => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => {
+      setIsFlipped(!isFlipped);
+    };
+
     return (
-        <div className="card" key={id}>
-          <h4>{ front }</h4>
+        <div className="card" key={id} onClick={ handleFlip }>
+          <h4>{ isFlipped ? back : front }</h4>
           <div className="button-container">
             <button className="delete-button" onClick={() => handleDelete(id)}>delete</button>
             <button className="edit-button">update</button>
