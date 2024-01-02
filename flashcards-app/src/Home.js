144481b68b1d9cd9895552b3c510ebd72ea6d162
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-01-02 22:33:13 +0400

    set time out for leading the fetched data

--- flashcards-app/src/Home.js
+++ flashcards-app/src/Home.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import CardsList from "./CardsList";
 
 const Home = () => {
-    const [cards, setCards] = useState(null)
+    const [cards, setCards] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     const handleDelete = (id) => {
         const newCards = cards.filter(card => card.id !== id);
@@ -10,17 +11,21 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch("http://localhost:8000/cards") //returns promise
+        setTimeout(() => {
+            fetch("http://localhost:8000/cards") //returns promise
             .then(res=> {
                 return res.json() //another promise
             })
             .then(data=> {
                 setCards(data);
-            })
+                setIsPending(false);
+            });
+        }, 1000)
     }, []);
     
     return (
         <div className="Home">
+            { isPending && <div style = {{ color: 'grey' }}>Loading...</div> }
            {cards && <CardsList cards={ cards } handleDelete={handleDelete}/>}
         </div>
     );
