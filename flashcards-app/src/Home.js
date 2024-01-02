e51dd2576f0737e2b4b5f36dd968132f5a52528e
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-01-02 22:50:33 +0400

    handle fetch errors

--- flashcards-app/src/Home.js
+++ flashcards-app/src/Home.js
@@ -4,6 +4,7 @@ import CardsList from "./CardsList";
 const Home = () => {
     const [cards, setCards] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     const handleDelete = (id) => {
         const newCards = cards.filter(card => card.id !== id);
@@ -12,21 +13,33 @@ const Home = () => {
 
     useEffect(() => {
         setTimeout(() => {
-            fetch("http://localhost:8000/cards") //returns promise
-            .then(res=> {
-                return res.json() //another promise
-            })
-            .then(data=> {
-                setCards(data);
-                setIsPending(false);
-            });
+            fetch("http://localhost:8001/cards") //returns promise
+                .then(res=> {
+                    if(res.ok) {
+                        throw Error('failed to fetch the data')
+                    }
+                    return res.json() //another promise
+                })
+                .then(data=> {
+                    setCards(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000)
     }, []);
     
     return (
         <div className="Home">
-            { isPending && <div style = {{ color: 'grey' }}>Loading...</div> }
-           {cards && <CardsList cards={ cards } handleDelete={handleDelete}/>}
+            <div className="pop-up-messages">
+                { error && <div>{ error }</div> }
+                { isPending && <div>Loading...</div> }
+            </div>
+            
+            { cards && <CardsList cards={ cards } handleDelete={handleDelete}/>}
         </div>
     );
 }
