a617e731609eb125ce0184c67784d2183d38700b
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2024-01-04 01:14:40 +0400

    fix some problems, update and style contact page

--- flashcards-app/src/Contact.js
+++ flashcards-app/src/Contact.js
@@ -1,72 +1,68 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// const Contact = () => {
-//   const [subject, setSubject] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [content, setContent] = useState('');
+const Contact = () => {
+  const [subject, setSubject] = useState('');
+  const [email, setEmail] = useState('');
+  const [content, setContent] = useState('');
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-  
-//     const messageData = {
-//       subject,
-//       email,
-//       content,
-//     };
-  
-//     try {
-//       const response = await fetch('http://localhost:8000/messages', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(messageData),
-//       });
-  
-//       if (response.ok) {
-//         console.log('Message sent successfully!');
-//       } else {
-//         console.error('Error sending message.');
-//       }
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-//   return (
-//     <div className="content">
-//       <div>Contact me</div>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Subject:
-//           <input
-//             type="text"
-//             value={subject}
-//             onChange={(e) => setSubject(e.target.value)}
-//           />
-//         </label>
-//         <br />
-//         <label>
-//           Email:
-//           <input
-//             type="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//         </label>
-//         <br />
-//         <label>
-//           Message:
-//           <textarea
-//             value={content}
-//             onChange={(e) => setContent(e.target.value)}
-//           />
-//         </label>
-//         <br />
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
+    const messageData = {
+      subject,
+      email,
+      content,
+    };
 
-// export default Contact;
+    await fetch('http://localhost:8000/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(messageData),
+    });
+    window.location.reload();
+  };
+
+  return (
+    <div className="content">
+      <div className="contact-header">Contact me</div>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <label>
+          Subject:
+          <input
+            type="text"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            className="input-field"
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input-field"
+          />
+        </label>
+        <br />
+        <label>
+          Message:
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="input-field"
+          />
+        </label>
+        <br />
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
